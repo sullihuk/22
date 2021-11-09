0130ee56d21e91c2246860ef3b8bb250af80607c
tdds <nyxa@nextmail.ru>
2021-11-10 06:46:35 +0700

    Handlers added

--- app.rb
+++ app.rb
@@ -5,3 +5,15 @@ require 'sinatra/contrib/all'
 get '/' do
   erb "<p>Hello Epta</p>"
 end
+
+get '/about' do
+  erb :about
+end
+
+get '/contacts' do
+  erb :contacts 
+end
+
+get '/visit' do
+  erb :visit
+end
