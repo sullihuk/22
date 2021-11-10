cc21d1670930d4ce8d4bc34ec57786a8e9fe2186
tdds <nyxa@nextmail.ru>
2021-11-11 06:26:44 +0700

    Capush

--- app.rb
+++ app.rb
@@ -17,3 +17,24 @@ end
 get '/visit' do
   erb :visit
 end
+
+post '/visit' do
+
+  @customer =params[:customer]
+  @date = params[:date]
+  @phone = params[:phone]
+  @barber = params[:barber]
+  dbase = File.open 'public/customers.txt', 'a'
+  dbase.puts  "#{@customer} | #{@date} | #{@phone} | #{@barber} \n"
+  dbase.close
+  erb :visit
+end
+
+post '/contacts' do
+  @email = params[:email]
+  @report = params[:report]
+  messages = File.open 'public/messages.txt', 'a'
+  messages.puts "#{@email} | #{@report}\n"
+  messages.close
+  erb "<p>Спасибо за ваш отзыв</p> <a href='/'>HOME</a>"
+end
